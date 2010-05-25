4403145207b6f0e63464e4e0700cbe07945ff367
Mark Erdmann <mark@crowdflower.com>
2010-05-24 17:48:46 -0700

    refactoring and adding webhook forwarding

--- image_moderator.rb
+++ image_moderator.rb
@@ -22,7 +22,7 @@ end
 $redis = Redis.new
 Resque.redis = $redis
 
-get '/api/imgs' do
+get '/api/imgs/:id' do
   
   content_type :json
   
@@ -32,38 +32,14 @@ get '/api/imgs' do
   job_id = $redis.get("image_moderator:#{key}")
   unit_id = $redis.get("image_moderator:jobs:#{job_id}:filename:#{filename}")
     
-  unit_data = HTTParty.get(
-    options.base_uri + "v1/jobs/#{job_id}/units/#{unit_id}.json?key=#{key}"
-    )
-  throw :halt, [401, "Not authenticated"] if unit_data.code == 401
-  throw :halt, [404, "Not found"] if unit_data.code == 404
-  agg = unit_data['results']['is_porn']['agg']
-  confidence = unit_data['results']['is_porn']['confidence']
-  
-  # use confidence as the rating if agg is true. if agg is false,
-  # the rating should be the inverse of confidence.
-  # (rating should be a value between 0 and 1, where 0 is definitely
-  # not porn and 1 is definitely porn)
-  if confidence
-    rating = agg == true ? confidence : 1 - confidence
-  else
-    rating = nil
-  end
-  
-  id = unit_data['id']
-  created_at = unit_data['created_at']
-  response = {  
-    :rating => rating, 
-    :id => id, 
-    :url => url, 
-    :created_at => created_at 
-    }
+  unit_data = get_unit_data(job_id, unit_id, key)
+  response = format_response(unit_data, url)
     
   response.to_json
   
 end
 
-post '/api/imgs' do
+post '/api/imgs/:id' do
   
   content_type :json
   
@@ -87,6 +63,45 @@ post '/api/imgs' do
   
 end
 
+post '/forward_webhook' do
+  
+  content_type :json
+  
+  client_webhook_uri = params[:webhook_uri]
+  puts client_webhook_uri
+  
+  if params[:signal] == 'unit_complete'
+  
+    unit_data = params[:payload]
+    puts unit_data.inspect
+    response = format_response(unit_data, unit_data['data']['url'])
+    
+    HTTParty.post(client_webhook_uri, :body => response)
+    
+  elsif params[:test]
+    
+    sample_data = {
+      :rating => 0,
+      :id => 2,
+      :url => "http://acme.crowdsifter.com/imgs/good.jpg",
+      :created_at => "2008-09-26T18:21:49Z"
+    }
+    HTTParty.post(client_webhook_uri, :body => sample_data)
+    
+  elsif params[:error]
+    
+    HTTParty.post(client_webhook_uri, :body => error)
+    
+  end
+  
+  
+end
+
+
+#############################################################################
+
+
+
 # this is here so that Resque knows how to queue the job
 class ProcessImage
   
@@ -97,5 +112,42 @@ end
 def authenticated?(key)
   
   HTTParty.get(options.base_uri + "v1/account.json?key=#{key}").code == 200
+  
+end
+
+def get_unit_data(job_id, unit_id, key)
+  
+  unit_data = HTTParty.get(
+    options.base_uri + "v1/jobs/#{job_id}/units/#{unit_id}.json?key=#{key}"
+    )
+  throw :halt, [401, "Not authenticated"] if unit_data.code == 401
+  throw :halt, [404, "Not found"] if unit_data.code == 404
+  unit_data
+  
+end
+
+def format_response(unit_data, url)
+  
+  agg = unit_data['results']['is_porn']['agg']
+  confidence = unit_data['results']['is_porn']['confidence']
+  
+  # use confidence as the rating if agg is true. if agg is false,
+  # the rating should be the inverse of confidence.
+  # (rating should be a value between 0 and 1, where 0 is definitely
+  # not porn and 1 is definitely porn)
+  if confidence
+    rating = agg == true ? confidence : 1 - confidence
+  else
+    rating = nil
+  end
+  
+  id = unit_data['id']
+  created_at = unit_data['created_at']
+  response = {  
+    :rating => rating, 
+    :id => id, 
+    :url => url, 
+    :created_at => created_at 
+    }
   
 end
--- tests.rb
+++ tests.rb
@@ -20,13 +20,23 @@ class ImageModeratorTest < Test::Unit::TestCase
   
   def assert_contains(string, *regexes)
     
-    regexes.each {|regex| assert last_response.body =~ /#{regex}/ }
+    regexes.each {|regex| assert string =~ regex }
+    
+  end
+  
+  def check_time(string)
+    
+    # this could fail if i didn't use the right strftime settings or
+    # if the time is set a second after the hour changes
+    hour = Time.now.gmtime.strftime("%H")
+    date = Time.now.gmtime.strftime("%b %d %Y")
+    assert_contains(string, /#{hour}:\d{2} #{date}/) 
     
   end
   
   def test_it_returns_a_401
     
-    post '/api/imgs', :url => "http://www.obvi.com/puppies.jpg", :token => '9532k4l329fs09fsjkl43'
+    post '/api/imgs/0', :url => "http://www.obvi.com/puppies.jpg", :token => '9532k4l329fs09fsjkl43'
     assert last_response.status == 401
     assert last_response.body == '{"error":"Authentication unsuccessful"}'
     
@@ -34,7 +44,10 @@ class ImageModeratorTest < Test::Unit::TestCase
   
   def test_it_accepts_a_post
     
-    post '/api/imgs', :url => TEST_IMAGE, :token => API_KEY
+    # set a job (normally the AM would do this)
+    # the job must have the appropriate fields
+    $redis.set("image_moderator:#{API_KEY}", 10700)
+    post '/api/imgs/0', :url => TEST_IMAGE, :token => API_KEY
     assert last_response.status == 200
     assert last_response.body =~ /Image queued for processing/
     
@@ -42,10 +55,47 @@ class ImageModeratorTest < Test::Unit::TestCase
   
   def test_it_returns_the_result
     
-    get '/api/imgs', :url => TEST_IMAGE, :token => API_KEY
+    get '/api/imgs/0', :url => TEST_IMAGE, :token => API_KEY
     assert last_response.status == 200
     assert last_response.body =~ /rating/ 
-    assert_contains last_response.body, "rating", "created_at", "url", "id"
+    assert_contains last_response.body, /rating/, /created_at/, /url/, /id/
+    
+  end
+  
+  def test_webhook_sends_test_data
+    
+    
+    post '/forward_webhook?webhook_uri%3Dhttp%3A%2F%2Fwww.postbin.org%2F1jl062e&test%3Dtrue'
+    response_body = HTTParty.get("http://www.postbin.org/1jl062e").body
+    check_time(response_body)
+    assert last_response.status == 200
+    
+  end
+  
+  def test_webhook_sends_real_data
+    
+    payload = {
+      "results" =>
+        {
+          "judgments" => [],
+          "is_porn" => {"agg" => nil,"confidence" => 0.5}
+        },
+        "job_id" => 10700,
+        "state" => "new",
+        "created_at" => "2010-05-24T17:31:17-07:00",
+        "data" => 
+          {"url" => "http://localhost:4000/images/marketing/crowdflower_logo.png",
+            "thumbnail_url" => "http://s3.amazonaws.com/jday_test/1dce4fe73a108e7c05fc6aceb77881e5.png"
+          },
+        "difficulty" => 0,
+        "updated_at" => "2010-05-24T17:31:17-07:00",
+        "judgments_count" => 0,
+        "id" => 14239444
+    }
+    post '/forward_webhook?webhook_uri%3Dhttp%3A%2F%2Fwww.postbin.org%2F1jl062e', :signal => :unit_complete, :payload => payload.to_json
+    response_body = HTTParty.get("http://www.postbin.org/1jl062e").body
+    check_time(response_body)
+    assert last_response.status == 200
     
   end
   
